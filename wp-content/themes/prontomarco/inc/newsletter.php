477f6b4022bb32455b832d04512f234ff6120cc7
Admin <pablolires@gmail.com>
2021-10-19 16:04:46 -0300

    Agregado de plugins y traducciones de varias secciones

---
+++ wp-content/themes/prontomarco/inc/newsletter.php
@@ -0,0 +1,33 @@
+<!-- Newsletter -->
+<section class="newsletter container">
+
+	<div class="row">
+		<div class="col-md-12 text-center">
+
+			<form>
+
+				<div class="row">
+					<div class="col-lg-3">
+						<p>
+							Suscribite al NEWSLETTER y enterate de todas las novedades.
+						</p>
+					</div>
+
+					<div class="col-lg-3">
+						<input type="text" class="form-control" name="name" placeholder="Nombre">
+					</div>
+					<div class="col-lg-3">
+						<input type="email" class="form-control" name="email" placeholder="Email">
+					</div>
+					<div class="col-lg-3">
+						<button class="btn" type="submit">SUSCRIBIRSE</button>
+					</div>
+				</div>
+
+			</form>
+
+		</div>
+	</div>
+
+</section>
+<!-- Newsletter end -->
---
+++ wp-content/themes/prontomarco/page-contacto.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<!-- FAQS -->
+<section class="prontomarco_faqs">
+
+	<!-- Productos -->
+	<section class="mas_vistos container">
+
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<h1>CONTACTO</h1>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam nesciunt odio temporibus doloribus aut, iure adipisci eius ducimus dignissimos vero in provident accusantium et dolorum, sunt labore quia necessitatibus iusto.
+			</div>
+		</div>
+
+	</section>
+	<!-- Productos end -->
+
+</section>
+<!-- FAQS end -->
+
+<?php get_footer(); ?>
