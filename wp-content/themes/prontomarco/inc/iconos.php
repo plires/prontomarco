2e8ee4ba04442ffa889e602377d4e4119bdb0ac3
Admin <pablolires@gmail.com>
2021-10-26 16:23:54 -0300

    Agregado de seccion contacto

---
+++ wp-content/themes/prontomarco/inc/iconos.php
@@ -0,0 +1,42 @@
+<!-- Iconos -->
+<section class="iconos container">
+
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<img 
+	      	class="img-fluid arrow" 
+	      	src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/flecha-abajo-gr.png'; ?>" 
+	      	alt="flecha abajo grande iconos">
+		</div>
+	</div>
+
+	<div class="row">
+
+		<div class="col-4 text-center">
+			<img 
+	      	class="img-fluid" 
+	      	src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/envios.png'; ?>" 
+	      	alt="envios a todo el pais">
+		    <h4>ENVÍOS A <br>TODO EL PAÍS</h4>
+		</div>
+
+		<div class="col-4 text-center">
+			<img 
+	      	class="img-fluid" 
+	      	src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/pagos.png'; ?>" 
+	      	alt="todos los medios de pago">
+		    <h4>ACEPTAMOS TODO <br>TIPO DE PAGO</h4>
+		</div>
+
+		<div class="col-4 text-center">
+			<img 
+	      	class="img-fluid" 
+	      	src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/compra.png'; ?>" 
+	      	alt="compra segura">
+		    <h4>TU COMPRA <br>SEGURA</h4>
+		</div>
+
+	</div>
+
+</section>
+<!-- Iconos end -->
--- wp-content/themes/prontomarco/page-contacto.php
+++ wp-content/themes/prontomarco/page-contacto.php
@@ -1,22 +1,87 @@
 <?php get_header(); ?>
 
-<!-- FAQS -->
-<section class="prontomarco_faqs">
+<!-- Contacto -->
+<section class="prontomarco_contacto">
 
-	<!-- Productos -->
-	<section class="mas_vistos container">
-
+	<!-- Header -->
+	<section class="header container-fluid">
 		<div class="row">
 			<div class="col-md-12 text-center">
-				<h1>CONTACTO</h1>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam nesciunt odio temporibus doloribus aut, iure adipisci eius ducimus dignissimos vero in provident accusantium et dolorum, sunt labore quia necessitatibus iusto.
+				<img class="img-fluid" 
+					src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/contacto/header-contacto.jpg'; ?>" 
+			      	alt="header contacto">
 			</div>
 		</div>
-
 	</section>
-	<!-- Productos end -->
+	<!-- Header end -->
+
+	<!-- Formulario -->
+	<section class="formulario container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<h1>CONTACTANOS</h1>
+				<img 
+					class="img-fluid transition arrow" 
+					src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/flecha-abajo.png'; ?>" 
+			      	alt="flecha abajo categorias">
+			</div>
+
+			<div class="col-md-12">
+
+				<form method="post">
+
+					<!-- Nombre -->
+					<div class="form-group">
+						<label for="name">Nombre</label>
+						<input type="text" class="form-control" name="name" placeholder="Ej: Juan Perez">
+					</div>
+					<!-- Nombre end -->
+
+					<!-- Email -->
+					<div class="form-group">
+						<label for="email">Email</label>
+						<input type="email" class="form-control" name="email" placeholder="ej: juanperez@gmail.com">
+					</div>
+					<!-- Email end -->
+
+					<!-- Telefono -->
+					<div class="form-group">
+						<label for="phone">Teléfono</label>
+						<input type="tel" class="form-control" name="phone" placeholder="ej: 11 5054 2415">
+					</div>
+					<!-- Telefono end -->
+
+					<!-- Comentarios -->
+					<div class="form-group">
+						<label for="comments">Comentarios</label>
+						<textarea class="form-control" name="comments" rows="5" placeholder="ej: Necesito información..."></textarea>
+					</div>
+					<!-- Comentarios end -->
+					
+					<button type="submit" class="btn btn-primary">ENVIAR</button>
+
+				</form>
+
+			</div>	
+		</div>
+	</section>
+	<!-- Formulario end -->
+
+	<!-- Mapa -->
+	<section class="mapa container-fluid">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3288.3615368260157!2d-58.55373738477326!3d-34.493717580488166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb076560a8349%3A0x638ecb452b0b2a04!2sPronto%20Marco!5e0!3m2!1ses-419!2sar!4v1635272350599!5m2!1ses-419!2sar" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+			</div>
+		</div>
+	</section>
+	<!-- Mapa end -->
+
+	<?php include('inc/iconos.php'); ?>	
+
+	<?php include('inc/newsletter.php'); ?>
 
 </section>
-<!-- FAQS end -->
+<!-- Contacto end -->
 
 <?php get_footer(); ?>
